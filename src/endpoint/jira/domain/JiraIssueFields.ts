bf0b6f987115fd3496fb1a8cd0ce25e997536d5b
Philippe Schild <Philippe.Schild@adesso.de>
2018-11-12 10:58:36 +0100

    added enum

--- src/endpoint/jira/domain/JiraIssueFields.ts
+++ src/endpoint/jira/domain/JiraIssueFields.ts
@@ -1,8 +1,12 @@
 import { JiraIssueAssignee } from './JiraIssueAssignee';
 import { Type } from 'class-transformer';
 
+export enum IssueType {
+    BUG = 'Bug'
+}
+
 export class JiraIssueFields {
-    issuetype: {id: string, name: string};
+    issuetype: {id: string, name: IssueType};
 
     @Type(() => JiraIssueAssignee)
     assignee: JiraIssueAssignee;
--- test/JiraEndpointController.spec.ts
+++ test/JiraEndpointController.spec.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import { Container } from 'typescript-ioc';
 import { JiraEndpointController } from '../src/endpoint/jira/JiraEndpointController';
 import { JiraIssue } from '../src/endpoint/jira/domain/JiraIssue';
+import { IssueType } from '../src/endpoint/jira/domain/JiraIssueFields';
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ describe('JiraEndpointController', () => {
         const issue: JiraIssue = await this.controller.getIssue(process.env.TEST_ISSUE_ID);
 
         expect(issue.key).toBe(process.env.TEST_ISSUE_ID);
-        expect(issue.fields.issuetype.name).toBe('Bug');
+        expect(issue.fields.issuetype.name).toBe(IssueType.BUG);
         expect(issue.fields.assignee.displayName).toBeDefined();
         expect(issue.getAssignee()).toEqual(issue.fields.assignee);
     });

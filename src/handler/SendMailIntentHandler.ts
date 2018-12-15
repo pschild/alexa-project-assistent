f8acb78ce875ca76db48f79b4e096820e4943b53
Philippe Schild <Philippe.Schild@adesso.de>
2018-12-15 20:56:16 +0100

    moved code for handling elicitation and confirmation of slots in util functions

--- src/handler/SendMailIntentHandler.ts
+++ src/handler/SendMailIntentHandler.ts
@@ -5,104 +5,78 @@ import AppState from '../app/state/AppState';
 import EmployeeState from '../app/state/EmployeeState';
 import { buildSuccessNotification, buildErrorNotification, buildWarningNotification } from '../apl/datasources';
 import { HandlerError } from '../error/HandlerError';
+import { elicitSlot, ElicitationStatus, confirmSlot, ConfirmationStatus } from './handlerUtils';
 
 export default async (request: alexa.request, response: alexa.response): Promise<alexa.response> => {
     const appState: AppState = Container.get(AppState);
     const employeeState: EmployeeState = appState.getEmployeeState();
-    const controller: MailController = Container.get(MailController);
     let activeEmployee = employeeState.getActive();
+    const controller: MailController = Container.get(MailController);
 
-    // beginning of the dialog: prefill MailRecipient slot with current user (from state, if any)
-    // futhermore, make sure MailContent slot is filled
-    if (request.getDialog().isStarted()) {
-        const updatedIntent = request.data.request.intent;
-        if (activeEmployee) {
-            updatedIntent.slots.MailRecipient = {
-                name: 'MailRecipient',
-                value: activeEmployee.name,
-                confirmationStatus: 'NONE'
-            };
-        }
+    const contentElicitationResult = elicitSlot(request, 'MailContent');
+    if (contentElicitationResult.status === ElicitationStatus.MISSING) {
         return response
-            .directive({
-                type: 'Dialog.Delegate',
-                updatedIntent
-            })
+            .say(`Was soll ich dir schreiben?`)
+            .directive(contentElicitationResult.directive)
             .shouldEndSession(false);
-
-        // manually ensure that MailRecipient slot is filled
-        // also re-ask for MailRecipient when a recipient was denied by the user
-    } else if (!request.slot('MailRecipient') || request.slots.MailRecipient.confirmationStatus === 'DENIED') {
-        employeeState.removeActive();
-        const updatedIntent = request.data.request.intent;
-        return response
-            .say(`An wen soll die Mail gehen?`)
-            .directive({
-                type: 'Dialog.ElicitSlot',
-                slotToElicit: 'MailRecipient',
-                updatedIntent
-            })
-            .shouldEndSession(false);
-
-        // make sure that given MailRecipient is a valid user
-        // otherwise, re-ask for MailRecipient
-    } else if (request.slots.MailRecipient.resolution()) {
-        if (!request.slots.MailRecipient.resolution().isMatched()) {
-            const updatedIntent = request.data.request.intent;
-            return response
-                .say(`${request.slot('MailRecipient')} kenne ich nicht. Kannst du mir den Vor und Nachnamen des Empfängers nochmal nennen?`)
-                .reprompt(`Der Mitarbeiter ${request.slot('MailRecipient')} ist mir nicht bekannt. Kannst du mir den Namen nochmal sagen?`)
-                .directive({
-                    type: 'Dialog.ElicitSlot',
-                    slotToElicit: 'MailRecipient',
-                    updatedIntent
-                })
-                .shouldEndSession(false);
-        }
     }
 
-    // get recipient name either from local state or from slot
-    let recipientName: string;
-    if (!activeEmployee) {
-        recipientName = request.slots.MailRecipient.resolution().values[0].name;
-    } else {
-        recipientName = activeEmployee.name;
+    const recipientElicitationResult = elicitSlot(request, 'MailRecipient', true, activeEmployee ? activeEmployee.name : undefined);
+    if (recipientElicitationResult.status !== ElicitationStatus.SUCCESS) {
+        if (recipientElicitationResult.status === ElicitationStatus.MISSING) {
+            response.say(`An wen soll die Mail gehen?`);
+        } else if (recipientElicitationResult.status === ElicitationStatus.UNMATCHED) {
+            response
+                .say(`${recipientElicitationResult.value} kenne ich nicht. Kannst du mir den Vor und Nachnamen des Empfängers nochmal nennen?`)
+                .reprompt(`Der Mitarbeiter ${recipientElicitationResult.value} ist mir nicht bekannt. Kannst du mir den Namen nochmal sagen?`);
+        }
+        return response
+            .directive(recipientElicitationResult.directive)
+            .shouldEndSession(false);
     }
 
-    // ensure confirmation of MailRecipient before actually sending the mail
-    if (!request.slots.MailRecipient.isConfirmed()) {
-        const updatedIntent = request.data.request.intent;
+    const recipientConfirmation = confirmSlot(request, 'MailRecipient');
+    if (recipientConfirmation.status !== ConfirmationStatus.CONFIRMED) {
+        if (recipientConfirmation.status === ConfirmationStatus.NONE) {
+            response
+                .say(`Die Mail geht an ${recipientElicitationResult.value}. Ist das OK?`)
+                .reprompt(`Ist ${recipientElicitationResult.value} der korrekte Empfänger?`);
+        } else if (recipientConfirmation.status === ConfirmationStatus.DENIED) {
+            employeeState.removeActive();
+            response.say(`Okay. An wen soll die Mail dann gehen?`);
+        }
         return response
-            .say(`Die Mail geht an ${recipientName}. Ist das OK?`)
-            .reprompt(`Ist ${recipientName} der korrekte Empfänger?`)
-            .directive({
-                type: 'Dialog.ConfirmSlot',
-                slotToConfirm: 'MailRecipient',
-                updatedIntent
-            })
+            .directive(recipientConfirmation.directive)
             .shouldEndSession(false);
     }
 
     try {
-        // set employee as the active one in state
-        employeeState.setActive(recipientName);
+        employeeState.setActive(recipientElicitationResult.value);
         activeEmployee = employeeState.getActive();
     } catch (error) {
         throw new HandlerError(
-            `${recipientName} wurde nicht in der Liste der Mitarbeiter gefunden.`,
-            buildWarningNotification('Mitarbeiter nicht gefunden', `${recipientName} wurde nicht in der Liste der Mitarbeiter gefunden.`)
+            `${recipientElicitationResult.value} wurde nicht in der Liste der Mitarbeiter gefunden.`,
+            buildWarningNotification(
+                'Mitarbeiter nicht gefunden',
+                `${recipientElicitationResult.value} wurde nicht in der Liste der Mitarbeiter gefunden.`
+            )
         );
     }
 
     if (!activeEmployee.email) {
-        throw new HandlerError(`Für ${activeEmployee.name} ist keine E-Mail-Adresse hinterlegt.`);
+        throw new HandlerError(
+            `Für ${activeEmployee.name} ist keine E-Mail-Adresse hinterlegt.`,
+            buildWarningNotification('Hinweis', `Für ${activeEmployee.name} ist keine E-Mail-Adresse hinterlegt.`)
+        );
     } else if (!activeEmployee.enableEmail) {
-        throw new HandlerError(`Für ${activeEmployee.name} ist der Versand von E-Mails deaktiviert.`);
+        throw new HandlerError(
+            `Für ${activeEmployee.name} ist der Versand von E-Mails deaktiviert.`,
+            buildWarningNotification('Hinweis', `Für ${activeEmployee.name} ist der Versand von E-Mails deaktiviert.`)
+        );
     }
 
-    const mailContent = request.slot('MailContent');
     try {
-        await controller.send(activeEmployee, mailContent);
+        await controller.send(activeEmployee, contentElicitationResult.value);
     } catch (error) {
         throw new HandlerError(
             `Beim Senden der Mail ist etwas schief gelaufen.`,
---
+++ src/handler/handlerUtils.ts
@@ -0,0 +1,96 @@
+import * as alexa from 'alexa-app';
+
+export enum ElicitationStatus {
+    SUCCESS = 'success',
+    MISSING = 'missing',
+    UNMATCHED = 'unmatched'
+}
+
+export enum ConfirmationStatus {
+    NONE = 'none',
+    CONFIRMED = 'confirmed',
+    DENIED = 'denied'
+}
+
+export interface ISlotElicitationResult {
+    status: ElicitationStatus;
+    value?: string;
+    directive?: any;
+}
+
+export interface ISlotConfirmationResult {
+    status: ConfirmationStatus;
+    directive?: any;
+}
+
+export const elicitSlot = (
+    request: alexa.request,
+    slotName: string,
+    mustMatch?: boolean,
+    prefilledValue?: string
+): ISlotElicitationResult => {
+    const updatedIntent = request.data.request.intent;
+    const slot = request.slots[slotName];
+
+    if (prefilledValue) {
+        return { status: ElicitationStatus.SUCCESS, value: prefilledValue };
+    }
+
+    if (slot && slot.value) {
+        if (mustMatch) {
+            if (slot.resolution() && slot.resolution().isMatched()) {
+                return { status: ElicitationStatus.SUCCESS, value: slot.resolution().values[0].name };
+            } else {
+                return {
+                    status: ElicitationStatus.UNMATCHED,
+                    value: slot.value,
+                    directive: {
+                        type: 'Dialog.ElicitSlot',
+                        slotToElicit: slotName,
+                        updatedIntent
+                    }
+                };
+            }
+        } else {
+            return { status: ElicitationStatus.SUCCESS, value: slot.value };
+        }
+    } else {
+        return {
+            status: ElicitationStatus.MISSING,
+            directive: {
+                type: 'Dialog.ElicitSlot',
+                slotToElicit: slotName,
+                updatedIntent
+            }
+        };
+    }
+};
+
+export const confirmSlot = (request: alexa.request, slotName: string): ISlotConfirmationResult => {
+    const updatedIntent = request.data.request.intent;
+    const slot = request.slots[slotName];
+
+    if (slot.isConfirmed()) {
+        return { status: ConfirmationStatus.CONFIRMED };
+    } else {
+        if (slot.confirmationStatus === 'NONE') {
+            return {
+                status: ConfirmationStatus.NONE,
+                directive: {
+                    type: 'Dialog.ConfirmSlot',
+                    slotToConfirm: slotName,
+                    updatedIntent
+                }
+            };
+        } else if (slot.confirmationStatus === 'DENIED') {
+            return {
+                status: ConfirmationStatus.DENIED,
+                directive: {
+                    type: 'Dialog.ElicitSlot',
+                    slotToElicit: slotName,
+                    updatedIntent
+                }
+            };
+        }
+    }
+};
